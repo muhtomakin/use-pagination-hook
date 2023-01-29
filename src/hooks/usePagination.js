83863712bab775d124a5132d29819094c870cd43
muhtomakin <muhtomakin@gmail.com>
2023-01-29 22:53:00 +0100

    add-new-features

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,36 +1,7 @@
-// import React from 'react';
+import React, { useState, useEffect } from "react";
 import { useMemo } from 'react';
 
 export const DOTS = "...";
-
-// function usePagination({
-//   currentPage,
-//   totalCount,
-//   pageSize,
-// }) {
-//     const paginationRange = useMemo(() => {
-
-//       const totalPageCount = Math.ceil(totalCount/pageSize)
-
-//       if (currentPage <= 3) {
-//         const lastPageItem = totalPageCount
-//         return [1, 2, 3, DOTS, lastPageItem]
-//       } else if (currentPage > 3 && currentPage <= totalPageCount-2) {
-//         const middlePageItem = currentPage
-//         return [1, DOTS, middlePageItem-1, middlePageItem, middlePageItem+1, DOTS, totalPageCount]
-//       } else {
-//         const lastPageItem = totalPageCount
-//         return [1, DOTS, lastPageItem-2, lastPageItem-1, lastPageItem]
-//       }
-//     }, [currentPage, totalCount, pageSize])
-
-
-
-//   return paginationRange;
-// }
-
-// export default usePagination;
-import React, { useState, useEffect } from "react";
 
 function usePagination(data, itemsPerPage) {
     const [currentPage, setCurrentPage] = useState(1);
